8345211e8b3801184f30475afa6564d4f98e37a7
Rone Silveira <ronebs10@hotmail.com>
2021-03-30 16:40:54 -0300

    redirecting users to the home if they dont have a subscription

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import { query as q } from 'faunadb'
 
 import NextAuth from 'next-auth';
+
 import Providers from 'next-auth/providers'
 
 import { fauna } from '../../../services/fauna'
@@ -12,10 +13,45 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       scope: 'read:user'
     }),
-
   ],
 
   callbacks: {
+    async session(session) {
+      
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])
+          )
+        )
+        return {
+          ...session,
+          activeSubsciption: userActiveSubscription
+        } 
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        }
+      }
+    },
     async signIn(user, account, profile) {
       const { email } = user
 
